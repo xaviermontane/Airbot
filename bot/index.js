9e5b7e3d88a8043f1ceb02a0987fc3a7b2b900be
xaviermontane <xaviermontane3@gmail.com>
2024-07-14 22:42:18 +0200

    Initial commit

---
+++ bot/index.js
@@ -0,0 +1,20 @@
+import { REST, Routes } from 'discord.js';
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
