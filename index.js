93939c3276e6a9aeae05be8cfbdfc61815428f00
xaviermontane <xaviermontane3@gmail.com>
2024-07-15 02:27:23 +0200

    Changes to be committed: 	modified:   .gitignore 	renamed:    bot/ -> Airbot/ 	new file:   commands/utility/ping.js 	new file:   commands/utility/server.js 	new file:   commands/utility/user.js 	new file:   deploy-commands.js 	new file:   events/interactionCreate.js 	new file:   events/ready.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+// Create a new client (bot) instance
+const client = new Client({
+	intents: [
+	  GatewayIntentBits.Guilds,
+	  GatewayIntentBits.GuildMessages,
+	  GatewayIntentBits.GuildMembers,
+	  GatewayIntentBits.DirectMessages,
+	],
+});
+
+client.commands = new Collection(); 
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) { 
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // Sync files
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Log in with the bot's token
+client.login(process.env.token);
+console.log('API key loaded...');
